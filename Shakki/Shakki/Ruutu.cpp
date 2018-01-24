26948ba2dad0757df77e57944bb785d3cc8ee9bd
Smokeylope <vahatom@gmail.com>
2018-01-24 11:15:14 +0200

    Ruutu ja Siirto

--- Shakki/Shakki/Ruutu.cpp
+++ Shakki/Shakki/Ruutu.cpp
@@ -1,3 +1,15 @@
 #include "Ruutu.h"
 
+Ruutu::Ruutu(int rivi, int sarake) {
+	_rivi = rivi;
+	_sarake = sarake;
+}
+
+int Ruutu::getRivi() {
+	return _rivi;
+}
+
+int Ruutu::getSarake() {
+	return _sarake;
+}
 
--- Shakki/Shakki/Ruutu.h
+++ Shakki/Shakki/Ruutu.h
@@ -1,13 +1,12 @@
 #pragma once
 
-class Ruutu
-{
+class Ruutu {
 public:
 	Ruutu(int rivi, int sarake);
 	Ruutu(){}
 	int getRivi();
 	int getSarake();
 private:
-	int sarake;
-	int rivi;
+	int _sarake;
+	int _rivi;
 };
