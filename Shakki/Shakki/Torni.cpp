15e20ea6c04bbbdbb685b49df2d21eff98ccd0fb
Smokeylope <vahatom@gmail.com>
2018-01-31 14:35:09 +0200

    Torni

---
+++ Shakki/Shakki/Torni.cpp
@@ -0,0 +1,43 @@
+#include "Torni.h"
+
+void Sotilas::annaSiirrot(std::list<Siirto>& lista, Ruutu* alkuRuutu, Asema* asema, int vari) {
+	// Ylös
+	for (int i = alkuRuutu->getRivi() + 1; i < 8; i++) {
+		if (asema->lauta[i][alkuRuutu->getSarake]) {
+			lista.push_back(Siirto(*alkuRuutu, Ruutu(i, alkuRuutu->getSarake())));
+		}
+		else {
+			break;
+		}
+	}
+
+	// Alas
+	for (int i = alkuRuutu->getRivi() - 1; i >= 0; i--) {
+		if (asema->lauta[i][alkuRuutu->getSarake]) {
+			lista.push_back(Siirto(*alkuRuutu, Ruutu(i, alkuRuutu->getSarake())));
+		}
+		else {
+			break;
+		}
+	}
+
+	// Oikealle
+	for (int i = alkuRuutu->getSarake() + 1; i < 8; i++) {
+		if (asema->lauta[alkuRuutu->getRivi()][i]) {
+			lista.push_back(Siirto(*alkuRuutu, Ruutu(i, alkuRuutu->getSarake())));
+		}
+		else {
+			break;
+		}
+	}
+
+	// Vasemmalle
+	for (int i = alkuRuutu->getSarake() - 1; i >= 0; i--) {
+		if (asema->lauta[alkuRuutu->getRivi()][i]) {
+			lista.push_back(Siirto(*alkuRuutu, Ruutu(i, alkuRuutu->getSarake())));
+		}
+		else {
+			break;
+		}
+	}
+}
