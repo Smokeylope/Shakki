d6f3f255eec27a4db85ccf364dfacd177db0198d
MickeyMike <mikaru@metropolia.fi>
2018-02-06 15:54:09 +0200

    Sotilas lyominen

--- Shakki/Shakki/Sotilas.cpp
+++ Shakki/Shakki/Sotilas.cpp
@@ -3,8 +3,7 @@
 void Sotilas::annaSiirrot(std::list<Siirto>& lista, Ruutu* alkuRuutu, Asema* asema, int vari) {
 	if (vari == 0) {
 		if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() + 1, alkuRuutu->getSarake())) {
-			lista.push_back(Siirto(*alkuRuutu,
-				Ruutu(alkuRuutu->getRivi() + 1, alkuRuutu->getSarake())));
+			lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() + 1, alkuRuutu->getSarake())));
 
 			if (alkuRuutu->getRivi() == 1) {
 				if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() + 2, alkuRuutu->getSarake())) {
@@ -12,7 +11,14 @@ void Sotilas::annaSiirrot(std::list<Siirto>& lista, Ruutu* alkuRuutu, Asema* ase
 				}
 			}
 		}
-		
+		// Onko lyöntimahdollisuutta oikea puoli
+		if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() + 1, alkuRuutu->getSarake() + 1)) {
+			lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() + 1, alkuRuutu->getSarake() + 1)));
+		}
+		// Onko lyöntimahdollisuutta vasen puoli
+		if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() + 1, alkuRuutu->getSarake() - 1)) {
+			lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() + 1, alkuRuutu->getSarake() - 1)));
+		}
 	}
 	else {
 		if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() - 1, alkuRuutu->getSarake())) {
@@ -24,6 +30,14 @@ void Sotilas::annaSiirrot(std::list<Siirto>& lista, Ruutu* alkuRuutu, Asema* ase
 					lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() - 2, alkuRuutu->getSarake())));
 				}
 			}
+		}
+		// Onko lyöntimahdollisuutta oikea puoli
+		if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() - 1, alkuRuutu->getSarake() + 1)) {
+			lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() - 1, alkuRuutu->getSarake() + 1)));
+		}
+		// Onko lyöntimahdollisuutta vasen puoli
+		if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() - 1, alkuRuutu->getSarake() - 1)) {
+			lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() - 1, alkuRuutu->getSarake() - 1)));
 		}
 	}
 }
