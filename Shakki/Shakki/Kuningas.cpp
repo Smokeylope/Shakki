e8a5a35ea7a715db3e7f024af6e2b7d92a4d7334
Smokeylope <vahatom@gmail.com>
2018-02-06 15:12:29 +0200

    Kuningas

--- Shakki/Shakki/Kuningas.cpp
+++ Shakki/Shakki/Kuningas.cpp
@@ -1,12 +1,35 @@
 #include "Kuningas.h"
 
 void Kuningas::annaSiirrot(std::list<Siirto>& lista, Ruutu* alkuRuutu, Asema* asema, int vari) {
-	lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi(), alkuRuutu->getSarake() + 1))); // Klo 12
-	lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() + 1, alkuRuutu->getSarake() + 1)));
-	lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() + 1, alkuRuutu->getSarake())));
-	lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() + 1, alkuRuutu->getSarake() - 1)));
-	lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi(), alkuRuutu->getSarake() - 1))); // Klo 6
-	lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() - 1, alkuRuutu->getSarake() - 1)));
-	lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() - 1, alkuRuutu->getSarake())));
-	lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() - 1, alkuRuutu->getSarake() + 1)));
+	if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi(), alkuRuutu->getSarake() + 1)) {
+		lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi(), alkuRuutu->getSarake() + 1))); // Klo 12
+	}
+	
+	if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() + 1, alkuRuutu->getSarake() + 1)) {
+		lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() + 1, alkuRuutu->getSarake() + 1)));
+	}
+
+	if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() + 1, alkuRuutu->getSarake())) {
+		lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() + 1, alkuRuutu->getSarake())));
+	}
+	
+	if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() + 1, alkuRuutu->getSarake() - 1)) {
+		lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() + 1, alkuRuutu->getSarake() - 1)));
+	}
+
+	if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi(), alkuRuutu->getSarake() - 1)) {
+		lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi(), alkuRuutu->getSarake() - 1))); // Klo 6
+	}
+
+	if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() - 1, alkuRuutu->getSarake() - 1)) {
+		lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() - 1, alkuRuutu->getSarake() - 1)));
+	}
+	
+	if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() - 1, alkuRuutu->getSarake())) {
+		lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() - 1, alkuRuutu->getSarake())));
+	}
+
+	if (onkoSiirtoMahdollinen(asema, alkuRuutu->getRivi() - 1, alkuRuutu->getSarake() + 1)) {
+		lista.push_back(Siirto(*alkuRuutu, Ruutu(alkuRuutu->getRivi() - 1, alkuRuutu->getSarake() + 1)));
+	}
 }
